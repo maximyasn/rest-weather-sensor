c60f339ea30c1bf842815fe3a66b46626ed235ce
maximyasn <maximyasniy11@gmail.com>
2023-11-26 18:17:49 +0300

    Initial commit

---
+++ src/test/java/com/maximyasn/restweathersensor/RestWeatherSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximyasn.restweathersensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeatherSensorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
